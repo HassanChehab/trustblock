0fbee1f8523adec3c5c7dc35f4144f0982772af2
h.chehab <ha.chehab@protonmail.com>
2023-06-24 00:57:01 +0200

    feat: instantiate client.

---
+++ client/src/shared/components/layout.tsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+	return (
+		<>
+			{/* header */}
+			<main>{children}</main>
+			<h1>hello world</h1>
+			{/* footer */}
+		</>
+	);
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // 'custom-red': '#FDD6DF',
+        // 'secondary': '#DAEAEE',
+        // 'terciary': '#CCF4E3',
+        // 'primary': '#E9DDD5',
+        // 'background': '#101436'
+      },
+      screens: {
+        xs: "0px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
