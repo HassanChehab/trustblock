ec370ce63a84f38d5b583895861c6451eb61db32
h.chehab <ha.chehab@protonmail.com>
2023-06-28 14:07:40 +0200

    feat: vercel deploy configuration.

--- client/next.config.js
+++ client/next.config.js
@@ -21,6 +21,8 @@ const nextConfig = {
 		SERVER_URL: "http://localhost:4000",
 		NEXTAUTH_SECRET: "superSecret!hello",
 		NEXTAUTH_FROM: "hassan.dev.smtp@gmail.com",
+		NEXTAUTH_URL:
+			"trustblock-front-g3n39k2qg-chehab91300-gmailcom.vercel.app",
 		NEXTAUTH_EMAIL_SERVER:
 			"smtp://hassan.dev.smtp@gmail.com:zwpcenusxbkvpzuv@smtp.gmail.com:587",
 	},
